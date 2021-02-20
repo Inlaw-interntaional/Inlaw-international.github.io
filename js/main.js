1be0fb06fb60c1e0bf08d8dff2627fb228b7040f
inlaw-international <78347491+inlaw-international@users.noreply.github.com>
2021-02-20 18:20:19 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -13,3 +13,28 @@ $(document).ready(function() {
         fade: true
     });
 });
+
+// Paralax 
+$(document).ready(function(){
+    var c1 = new Rellax('.intro', {
+     speed: -6
+    });
+   });
+
+// Плавное появление AOS
+AOS.init();
+
+// Якоря
+
+$(document).ready(function() {
+    var margin = -75; // переменная для контроля докрутки
+    $("a").click(function() { // тут пишите условия, для всех ссылок или для конкретных
+       $("html, body").animate({
+          scrollTop: $($(this).attr("href")).offset().top+margin+ "px" // .top+margin - ставьте минус, если хотите увеличить отступ
+       }, {
+          duration: 1600, // тут можно контролировать скорость
+          easing: "swing"
+       });
+       return false;
+    });
+ });
