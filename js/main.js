62e827c71cfb54f951f78574f9c741cbe2fb3f6d
inlaw-international <78347491+inlaw-international@users.noreply.github.com>
2021-02-21 18:07:10 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ jQuery.ajax({
     }
 }).done(function() {
     logo_animation_full();
+    anchors();
 });
 
 // LOGO animation
@@ -79,15 +80,17 @@ AOS.init();
 
 // Якоря
 
-$(document).ready(function() {
-    var margin = -75; // переменная для контроля докрутки
-    $("a").click(function() { // тут пишите условия, для всех ссылок или для конкретных
-        $("html, body").animate({
-            scrollTop: $($(this).attr("href")).offset().top + margin + "px" // .top+margin - ставьте минус, если хотите увеличить отступ
-        }, {
-            duration: 800, // тут можно контролировать скорость
-            easing: "swing"
+function anchors() {
+    $(document).ready(function() {
+        var margin = -75; // переменная для контроля докрутки
+        $("a").click(function() { // тут пишите условия, для всех ссылок или для конкретных
+            $("html, body").animate({
+                scrollTop: $($(this).attr("href")).offset().top + margin + "px" // .top+margin - ставьте минус, если хотите увеличить отступ
+            }, {
+                duration: 800, // тут можно контролировать скорость
+                easing: "swing"
+            });
+            return false;
         });
-        return false;
     });
-});
+}
