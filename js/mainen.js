2bf8feb04818ec7950d3009a71beb7701ed48063
inlaw-international <78347491+inlaw-international@users.noreply.github.com>
2021-03-04 11:05:40 +0200

    Add files via upload

--- js/mainen.js
+++ js/mainen.js
@@ -90,9 +90,32 @@ function anchors() {
     });
 }
 
+$('.dropdown').click(function () {
+    $(this).attr('tabindex', 1).focus();
+    $(this).toggleClass('active');
+    $(this).find('.dropdown-menu').slideToggle(300);
+});
+$('.dropdown').focusout(function () {
+    $(this).removeClass('active');
+    $(this).find('.dropdown-menu').slideUp(300);
+});
+$('.dropdown .dropdown-menu li').click(function () {
+    $(this).parents('.dropdown').find('span').text($(this).text());
+    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+});
+/*End Dropdown Menu*/
+
+
+$('.dropdown-menu li').click(function () {
+var input = '<strong>' + $(this).parents('.dropdown').find('input').val() + '</strong>',
+  msg = '<span class="msg">Hidden input value: ';
+$('.msg').html(msg + input + '</span>');
+}); 
+
+
 // Подвал
 jQuery.ajax({
-    url: "../footer.html?2",
+    url: "../footeren.html?2",
     dataType: "html",
     success: function(response) {
         document.getElementById('footer').innerHTML = response;
