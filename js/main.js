7f91eb6b210f6c3b8c728723dc6fd13bf64b37df
inlaw-international <78347491+inlaw-international@users.noreply.github.com>
2021-03-04 11:30:09 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -90,10 +90,10 @@ function anchors() {
     });
 }
 
-$('.dropdown').click(function () {
-    $(this).attr('tabindex', 1).focus();
-    $(this).toggleClass('active');
-    $(this).find('.dropdown-menu').slideToggle(300);
+$('.dropdown .select').click(function () {
+    $(".dropdown").attr('tabindex', 1).focus();
+    $(".dropdown").toggleClass('active');
+    $(".dropdown").find('.dropdown-menu').slideToggle(300);
 });
 $('.dropdown').focusout(function () {
     $(this).removeClass('active');
